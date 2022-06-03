3ba034e4c28d9289e816f6bcb17dafea87ca3f51
Alex Chechulov <alex.chechulov@gmail.com>
2022-06-03 12:14:58 +0300

    Добавьте файлы проекта.

---
+++ ConverterExsel/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConverterExsel
+{
+    public class Class1
+    {
+        public string converter(string path_begin, DateTime date_begin, DateTime date_end, string parsing_format, string path_end, string name)
+        {
+            string answer = "";
+            string path = Directory.GetCurrentDirectory() + "\\DATA\\" + path_begin+"\\";
+            string date_month = date_begin.Month < 10 ? "0" + date_begin.Month : date_begin.Month.ToString();
+            string date_day = date_begin.Day < 10 ? "0" + date_begin.Day : date_begin.Day.ToString();
+            string file_name = date_begin.Year + "-" +date_month + "-" + date_day + ".YML";
+            switch (parsing_format)
+            {
+                case "ArchivePob":
+                    using (StreamReader reader = new StreamReader(path+file_name))
+                    {
+                        List<string> radiation_data = new List<string>();
+                        List<string> time = new List<string>();
+                        var file = reader.ReadToEnd().ToString().Split(new char[] { '\n' });
+                        foreach (string line_of_file in file)
+                        {
+                            if (line_of_file != "")
+                            {
+                                var words = line_of_file.Split(new char[] { ' ' });
+                                time.Add(words[1].Substring(1, words[1].Length - 3));
+                                radiation_data.Add(words[3].Substring(0, words[3].Length - 2));
+                            }
+                        }
+                        //answer = line_of_file;
+
+
+                        for (int i = 0; i < time.Count; i++)
+                        {
+                            Console.WriteLine(time[i] + " radiation: " + radiation_data[i]);
+                        }
+
+                    }
+
+                    break;
+
+
+                case "ArchiveXPob":
+
+                    break;
+
+
+                case "ActinometryArchive":
+
+                    break;
+
+
+                case "MINpel":
+                case "Hpel":
+
+                    break;
+
+
+                case "VODpel":
+
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            return answer;
+        }
+    }
+}
---
+++ TestConverter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ConverterExsel;
+
+namespace TestConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConverterExsel.Class1 test = new Class1();
+            string test_ansver = test.converter("Port 8, Subchannel 0 - Пиранометр (СФ-06)", new DateTime(2022, 05, 06), DateTime.UtcNow, "ArchivePob", "/some else", "test");
+            //Console.WriteLine(test_ansver);
+            var words = test_ansver.Split(new char[] { ' ' });
+            //Console.WriteLine(words.Length);
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            //Console.WriteLine(words[1]);
+            //string time = words[1].Substring(1, words[1].Length - 3);
+            //Console.WriteLine(time);
+
+            Console.ReadLine();
+        }
+    }
+}
